a778304d189731e6d98d8ed4dda633a473c1bf4b
Safo <s.i.post@mai.ru>
2020-02-04 16:06:23 +0500

    add getInfoDeposit(), calcSavedMoney(), toUpperCase for appData.addExpenses

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ let appData = {
   expenses: {},
   addExpenses: [],
   deposit: false,
+  percentDeposit: 0,
+  moneyDeposit: 0,
   mission: 300000,
   period: 10,
   budget: money,
@@ -26,16 +28,34 @@ let appData = {
   budgetMonth: 0,
   expensesMonth: 0,
   asking: function () {
-    let valueExpenses = 0;
+    if (confirm('Есть ли у вас дополнительный источник зароботка?')) {
+      let itemIncome;
+      do {
+        itemIncome = prompt('Какой у вас дополнительный заработок?', 'Таксую');
+      } while (isNumber(itemIncome));
+      let cashIncome;
+      do {
+        cashIncome = prompt('Склолько в месяц вы на этом зарабатываете?', 10000);
+      } while (!isNumber(cashIncome));
+      appData.income[itemIncome] = cashIncome;
+    };
     for (let i = 0; i < 2; i++) {
-      let keyExpenses = prompt('Введите обязательную статью расходов?');
+      let keyExpenses;
+      do {
+        keyExpenses = prompt('Введите обязательную статью расходов?');
+      } while (isNumber(keyExpenses));
+      let valueExpenses = 0;
       do {
         valueExpenses = prompt('Во сколько это обойдется?');
       } while (!isNumber(valueExpenses));
       appData.expenses[keyExpenses] = +valueExpenses;
     };
     let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-    appData.addExpenses = addExpenses.toLowerCase().split(',');
+    appData.addExpenses = addExpenses.toLowerCase().split(', ');
+    for (let i = 0; i < appData.addExpenses.length; i++) {
+      appData.addExpenses[i] = appData.addExpenses[i].charAt(0).toUpperCase() + appData.addExpenses[i].substring(1);
+    };
+    console.log(appData.addExpenses);
     appData.deposit = confirm('Есть ли у вас депозит в банке?');
   },
   getExpensesMonth: function () { // Сумма обязательных расходов за месяц
@@ -50,12 +70,7 @@ let appData = {
     appData.budgetDay = Math.ceil(appData.budgetMonth / 30);
   },
   getTargetMonth: function () { // Период, за который цель будет достигнута
-    appData.period = Math.floor(appData.mission / appData.budgetMonth);
-    if (appData.period > 0) {
-      console.log('Цель будет достигнута за ' + appData.period + ' месяцев');
-    } else {
-      console.log('К сожалению, цель не будет достигнута');
-    }
+    return Math.floor(appData.mission / appData.budgetMonth);
   },
   getStatusIncome: function () { // Выводим уровень дохода
     if (appData.budgetDay >= 1200) {
@@ -67,6 +82,19 @@ let appData = {
     } else {
       return ('Что то пошло не так');
     };
+  },
+  getInfoDeposit: function () {
+    if (appData.deposit) {
+      do {
+        appData.percentDeposit = prompt('Какой годовой процент?', 10);
+      } while (!isNumber(appData.percentDeposit));
+      do {
+        appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+      } while (!isNumber(appData.moneyDeposit));
+    }
+  },
+  calcSavedMoney: function () {
+    return appData.budgetMonth * appData.period;
   }
 };
 
@@ -76,15 +104,22 @@ appData.expensesMonth = appData.getExpensesMonth();
 console.log('Обязательные расходы за месяц: ' + appData.expensesMonth);
 
 appData.getBudget();
+appData.getInfoDeposit();
+appData.calcSavedMoney();
 
-appData.getTargetMonth();
+if (appData.getTargetMonth() > 0) {
+  console.log('Цель будет достигнута за ' + appData.getTargetMonth() + ' месяцев');
+} else {
+  console.log('К сожалению, цель не будет достигнута');
+};
 
 console.log(appData.getStatusIncome());
 
 for (let key in appData) {
   console.log('Наша программа включает в себя данные:' +
-    '\n обязательные расходы за месяц: ' + appData.expenses() +
-    '\n возможные расходы: ' + appData.addExpenses +
+    '\n дополнительный заработок: ' + appData.income +
+    '\n обязательные расходы за месяц: ' + appData.expenses +
+    '\n возможные расходы за месяц: ' + appData.addExpenses +
     '\n наличие депозита в банке: ' + appData.deposit +
     '\n цель: ' + appData.mission +
     '\n период, за который цель будет достигнута: ' + appData.period +
